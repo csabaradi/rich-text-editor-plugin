3172ae05154a06e769d9e94139e92e8cd65a1518
Csaba <csaba.radi.bp@nttdata.com>
2017-12-10 19:42:31 +0200

    initial commit

---
+++ src/main/java/ro/nttdata/atlassian/jira/Footer.java
@@ -0,0 +1,39 @@
+package ro.nttdata.atlassian.jira;
+
+import com.atlassian.jira.template.soy.SoyTemplateRendererProvider;
+import com.atlassian.renderer.RenderContext;
+import com.atlassian.renderer.v2.RenderMode;
+import com.atlassian.renderer.v2.macro.BaseMacro;
+import com.atlassian.renderer.v2.macro.MacroException;
+import com.atlassian.soy.renderer.SoyException;
+import com.atlassian.soy.renderer.SoyTemplateRenderer;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+public class Footer extends BaseMacro {
+
+    private static final String FALLBACK_RENDER_OUTPUT = "{noformat}%s{noformat}";
+
+    private final SoyTemplateRenderer soyTemplateRenderer;
+
+    public Footer(final SoyTemplateRendererProvider soyTemplateRendererProvider) {
+        super();
+        this.soyTemplateRenderer = soyTemplateRendererProvider.getRenderer();
+    }
+
+    @Override
+    public boolean hasBody() {
+        return true;
+    }
+
+    @Override
+    public RenderMode getBodyRenderMode() {
+        return RenderMode.allow(RenderMode.F_NONE);
+    }
+
+    @Override
+    public String execute(Map<String, Object> parameters, String body, RenderContext renderContext) throws MacroException {
+        return "<div class=\"footer-macro\">" + body + "</div>";
+    }
+}
